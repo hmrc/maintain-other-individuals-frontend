42db3ea8e68d45a51af27efff5a8fc37f39725c5
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2026-01-23 09:24:47 +0000

    DDCE-7772- PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
   val bootstrapVersion = "9.19.0"
-  val mongoVersion = "2.10.0"
+  val mongoVersion = "2.11.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % mongoVersion,
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.27.0",
     "uk.gov.hmrc"             %% "domain-play-30"                         % "11.0.0",
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.3.0"
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.4.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
